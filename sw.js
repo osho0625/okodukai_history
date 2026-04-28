a64597ba72ef89a626a363d3eb5a8f0b2ed7428a
ryosuke.tsuji <ryosuke.tsuji.zs@kyocera.jp>
2026-04-28 12:14:36 +0900

    faet: PWAとして利用可能に

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'okozukai-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './child.html',
+  './settings.html',
+  './admin.html',
+  './manifest.json',
+  './2728.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// ネットワーク優先、失敗時にキャッシュ
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
